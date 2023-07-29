4dd185ff29d9869f8d81185bc69fb54bdc34dcd0
maymine <sm9365277@gmail.com>
2023-07-28 23:49:40 -0500

    added suppliers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :suppliers
   resources :banks
   devise_for :users
   
---
+++ db/migrate/20230729044714_create_suppliers.rb
@@ -0,0 +1,12 @@
+class CreateSuppliers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :suppliers do |t|
+      t.string :name
+      t.integer :nit
+      t.string :name_person_contact
+      t.integer :phone_number_person_contact, limit: 10
+
+      t.timestamps
+    end
+  end
+end
